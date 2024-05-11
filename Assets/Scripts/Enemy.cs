289423ea3e641eeb00d3006b50a9fbd1d8763588
Jearle88 <johnthomas.earle@gmail.com>
2024-05-11 16:59:20 -0700

    spawner

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,29 @@
+
+using UnityEngine;
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private float speed;
+
+    private float countdown = 5f;
+
+    private WaveSpawner waveSpawner;
+
+    private void Start()
+    {
+        waveSpawner = GetComponentInParent<WaveSpawner>();
+    }
+    public void Update()
+    {
+        transform.Translate(transform.forward * speed * Time.deltaTime);
+
+        countdown -= Time.deltaTime;
+
+        if (countdown <= 0)
+        {
+            Debug.Log("DESTROY");
+            Destroy(gameObject);
+
+            waveSpawner.waves[waveSpawner.currentWaveIndex].enemiesLeft--;
+        }
+    }
+}
