ab09682db3d3369b7235044eea1ac0327ee15fe8
aleal209 <aleal@uoregon.edu>
2024-06-06 17:42:52 -0700

    Fixes
    Statue of liberty placing right
    Tutorial text being in the right places
    Camera can lock so you cannot get lost
    Changed some prices (specifically legendary tower/bat trump) in an act of balancing
    made bat trump model larger

--- Assets/Scripts/CameraMove.cs
+++ Assets/Scripts/CameraMove.cs
@@ -3,19 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class cameraMove : MonoBehaviour
-{ 
+{
     // Update is called once per frame
     // basic camera mover
+    public float xmax;
+    public float xmin;
+    public float zmax;
+    public float zmin;
+    [System.NonSerialized]public float xtranslation;
+    [System.NonSerialized]public float ztranslation;
     void Update()
     {
-        float ytranslation = Input.GetAxis("Vertical") * 0.1f;
-        float xtranslation = Input.GetAxis("Horizontal") * 0.1f;
+        {
+            xtranslation = Input.GetAxis("Horizontal") * 0.1f;
+            ztranslation = Input.GetAxis("Vertical") * 0.1f;
+            if(xtranslation + transform.position.x > xmax || xtranslation + transform.position.x < xmin)
+            {
+                xtranslation = 0.0f;
+            }
+            if (ztranslation + transform.position.z > zmax || ztranslation + transform.position.z < zmin)
+            {
+                ztranslation = 0.0f;
+            }
+            // Make it move 10 meters per second instead of 10 meters per frame...
+            // translation *= Time.deltaTime;
+            // rotation *= Time.deltaTime;
 
-        // Make it move 10 meters per second instead of 10 meters per frame...
-        // translation *= Time.deltaTime;
-        // rotation *= Time.deltaTime;
-
-        // Move translation along the object's z-axis
-        transform.Translate(xtranslation, ytranslation, 0);
+            // Move translation along the object's z-axis and x axis
+            transform.Translate(xtranslation, ztranslation, 0);
+        }
     }
 }
